7a9a1c041691d012294edac6790c1921dff5fcfd
wszxdhr <wszxdhr@126.com>
2019-08-27 23:40:16 +0800

    feat:完善添加备份的功能

--- src/views/Home/Backups/AddBackups.vue
+++ src/views/Home/Backups/AddBackups.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home-backups-add">
-    <el-steps :space="200" :active="1" finish-status="success" simple style="margin-top: -20px;">
+  <el-dialog class="home-backups-add" title="Add Backup" :visible.sync="currentShow" width="720px">
+    <el-steps :space="100" :active="activeIndex" finish-status="success" simple style="margin-top: -20px; margin-bottom: 20px;">
       <el-step title="Complete Info">123</el-step>
       <el-step title="Choose config">345</el-step>
       <el-step title="Finish">456</el-step>
     </el-steps>
     <div class="home-backups-steps">
-      <div class="step">1</div>
-      <div class="step">1</div>
-      <div class="step">1</div>
-      <div class="step">1</div>
-      <div class="step">1</div>
-      <div class="step">1</div>
-      <div class="step">1</div>
+      <div class="step" v-show="activeIndex === 0">
+        <el-form ref="addBackupForm" :model="formData" label-width="100px">
+          <el-form-item label="Name" required>
+            <el-input v-model="formData.name" placeholder="Please input a name for backups."></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="step" v-show="activeIndex === 1">2</div>
+      <div class="step" v-show="activeIndex === 2">3</div>
     </div>
-  </div>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="cancel">Cancel</el-button>
+      <el-button type="primary" v-show="activeIndex !== 0" @click="prevStep">
+        <i class="el-icon-arrow-left"></i>
+        <span>Prev</span>
+      </el-button>
+      <el-button type="primary" v-show="activeIndex !== maxStep" @click="nextStep">
+        <span>Next</span>
+        <i class="el-icon-arrow-right"></i>
+      </el-button>
+      <el-button type="primary" v-show="activeIndex === maxStep" @click="confirm">
+        <span>Confirm</span>
+        <i class="el-icon-check"></i>
+      </el-button>
+    </span>
+  </el-dialog>
 </template>
+
+<script>
+const getFormData = () => ({
+  name: '',
+  files: []
+})
+export default {
+  name: 'HomeBackupsAdd',
+  props: {
+    show: {
+      default: false
+    }
+  },
+  data () {
+    return {
+      activeIndex: 0,
+      currentShow: false,
+      maxStep: 2,
+      formData: getFormData()
+    }
+  },
+  methods: {
+    nextStep () {
+      this.activeIndex = Math.min(this.maxStep, ++this.activeIndex)
+    },
+    prevStep () {
+      this.activeIndex = Math.max(0, --this.activeIndex)
+    },
+    confirm () {},
+    clear () {
+      this.formData = getFormData()
+    },
+    cancel () {
+      this.currentShow = false
+      this.clear()
+    }
+  },
+  watch: {
+    show (val) {
+      this.currentShow = val
+    },
+    currentShow (val) {
+      this.$emit('update:show', val)
+    }
+  }
+}
+</script>
--- src/views/Home/Backups/index.vue
+++ src/views/Home/Backups/index.vue
@@ -6,15 +6,7 @@
         <el-button style="float: right; padding: 3px 0" type="text" @click="showAddDialog = true">Add Backup</el-button>
       </div>
     </el-card>
-    <el-dialog title="Add Backup" :visible.sync="showAddDialog" width="720px">
-      <transition mode="out-in">
-        <add-backups v-if="showAddDialog"></add-backups>
-      </transition>
-<!--      <el-form ref="addBackupForm" :model="addBackupForm" label-width="100px">-->
-<!--        <el-form-item label="Name"></el-form-item>-->
-<!--        <el-form-item label="Choose config file to backup"></el-form-item>-->
-<!--      </el-form>-->
-    </el-dialog>
+    <add-backups :show.sync="showAddDialog"></add-backups>
   </section>
 </template>
 
