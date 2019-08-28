e4b683b9b82434ff461500c72a5ea7df75d9d89e
wszxdhr <wszxdhr@126.com>
2019-08-28 20:11:15 +0800

    feat:修改logo为可配置渲染，修改首页逻辑

--- src/components/Logo/index.vue
+++ src/components/Logo/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo">
-    <canvas ref="canvas"></canvas>
+    <canvas ref="canvas" :class="{shadow: hasShadow}"></canvas>
   </div>
 </template>
 
@@ -13,23 +13,19 @@ export default {
     },
     canvasHeight: {
       default: 60
+    },
+    hasShadow: {
+      default: false
     }
   },
   data () {
     return {
       ctx: null,
+      isPlaying: false,
       colors: ['#9fffeb', '#b4ffba', '#e6ffa8',
         '#ffcdac', '#ffa3af', '#9dc2ff'],
       // 三维数组，第一级：column，第二级：point，第三级：x,y
-      points: [
-        [[20, 30]],
-        [[30, 20], [30, 40]],
-        [[40, 15], [40, 45]],
-        [[50, 15], [50, 45]],
-        [[60, 20], [60, 40]],
-        [[70, 30]],
-        [[80, 20], [80, 40]]
-      ]
+      points: []
     }
   },
   mounted () {
@@ -37,72 +33,46 @@ export default {
     canvas.width = this.canvasWidth
     canvas.height = this.canvasHeight
     this.ctx = canvas.getContext('2d')
+    this.resetPoints()
     this.render()
   },
   methods: {
+    resetPoints () {
+      this.points = [
+        [[this.canvasWidth * 0.2, this.canvasHeight * 0.5]],
+        [[this.canvasWidth * 0.3, this.canvasHeight / 3], [this.canvasWidth * 0.3, this.canvasHeight * 2 / 3]],
+        [[this.canvasWidth * 0.4, this.canvasHeight / 4], [this.canvasWidth * 0.4, this.canvasHeight * 3 / 4]],
+        [[this.canvasWidth * 0.5, this.canvasHeight / 4], [this.canvasWidth * 0.5, this.canvasHeight * 3 / 4]],
+        [[this.canvasWidth * 0.6, this.canvasHeight / 3], [this.canvasWidth * 0.6, this.canvasHeight * 2 / 3]],
+        [[this.canvasWidth * 0.7, this.canvasHeight * 0.5]],
+        [[this.canvasWidth * 0.8, this.canvasHeight / 3], [this.canvasWidth * 0.8, this.canvasHeight * 2 / 3]]
+      ]
+    },
     render () {
       this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
       this.ctx.save()
-      this.ctx.beginPath()
-      // 第一节
-      this.ctx.moveTo(...this.points[0][0])
-      this.ctx.lineTo(...this.points[1][0])
-      this.ctx.lineTo(...this.points[1][1])
-      this.ctx.lineTo(...this.points[0][0])
-      this.ctx.closePath()
-      this.ctx.fillStyle = this.colors[0]
-      this.ctx.fill()
-      this.ctx.restore()
-      // 第二节
-      this.ctx.beginPath()
-      this.ctx.moveTo(...this.points[1][1])
-      this.ctx.lineTo(...this.points[2][1])
-      this.ctx.lineTo(...this.points[2][0])
-      this.ctx.lineTo(...this.points[1][0])
-      this.ctx.lineTo(...this.points[1][1])
-      this.ctx.closePath()
-      this.ctx.fillStyle = this.colors[1]
-      this.ctx.fill()
-      this.ctx.restore()
-      // 第三节
-      this.ctx.beginPath()
-      this.ctx.moveTo(...this.points[2][0])
-      this.ctx.lineTo(...this.points[2][1])
-      this.ctx.lineTo(...this.points[3][1])
-      this.ctx.lineTo(...this.points[3][0])
-      this.ctx.closePath()
-      this.ctx.fillStyle = this.colors[2]
-      this.ctx.fill()
-      this.ctx.restore()
-      // 第四节
-      this.ctx.beginPath()
-      this.ctx.moveTo(...this.points[3][0])
-      this.ctx.lineTo(...this.points[3][1])
-      this.ctx.lineTo(...this.points[4][1])
-      this.ctx.lineTo(...this.points[4][0])
-      this.ctx.closePath()
-      this.ctx.fillStyle = this.colors[3]
-      this.ctx.fill()
-      this.ctx.restore()
-      // 第五节
-      this.ctx.beginPath()
-      this.ctx.moveTo(...this.points[4][1])
-      this.ctx.lineTo(...this.points[4][0])
-      this.ctx.lineTo(...this.points[5][0])
-      this.ctx.closePath()
-      this.ctx.fillStyle = this.colors[4]
-      this.ctx.fill()
-      this.ctx.restore()
-      // 第六节
-      this.ctx.beginPath()
-      this.ctx.moveTo(...this.points[5][0])
-      this.ctx.lineTo(...this.points[6][0])
-      this.ctx.lineTo(...this.points[6][1])
-      this.ctx.closePath()
-      this.ctx.fillStyle = this.colors[5]
-      this.ctx.fill()
-      this.ctx.restore()
-      // this.ctx.moveTo()
+      if (this.points && this.points.length) {
+        for (let colIndex = 0; colIndex < this.points.length - 1; colIndex++) {
+          console.log(colIndex)
+          this.ctx.beginPath()
+          this.ctx.moveTo(...this.points[colIndex][0])
+          this.points[colIndex][1] && this.ctx.lineTo(...this.points[colIndex][1])
+          this.points[colIndex + 1][1] && this.ctx.lineTo(...this.points[colIndex + 1][1])
+          this.ctx.lineTo(...this.points[colIndex + 1][0])
+          this.ctx.closePath()
+          this.ctx.fillStyle = this.colors[colIndex]
+          this.ctx.fill()
+          this.ctx.restore()
+        }
+      }
+    }
+  },
+  watch: {
+    canvasWidth () {
+      this.resetPoints()
+    },
+    canvasHeight () {
+      this.resetPoints()
     }
   }
 }
@@ -115,6 +85,9 @@ export default {
     canvas {
       width: 100%;
       height: 100%;
+      &.shadow {
+        filter: drop-shadow(0 0 30px rgba(0, 0, 0, 0.1));
+      }
     }
   }
 </style>
--- src/views/Home/Index/index.vue
+++ src/views/Home/Index/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <section class="home-index">
-    <logo :canvas-width="600" :canvas-height="360"></logo>
-  </section>
+  <div class="home-index">
+    <el-card class="home-index-card">
+      <section class="home-index-logo-row">
+        <logo :canvas-width="400" :canvas-height="240" :has-shadow="true"></logo>
+        <div class="home-index-logo-row_text">
+          <p class="title">Mofish</p>
+          <p class="description">A friendly and open source Development Tool.</p>
+        </div>
+      </section>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -15,13 +23,37 @@ export default {
 </script>
 
 <style lang="scss">
+  @import "~@/assets/style/base.scss";
   .home-index {
-    position: relative;
-    .logo {
-      margin-left: 50%;
-      width: 600px;
-      height: 360px;
-      transform: translateX(-50%);
+    .home-index-card {
+      position: relative;
+      max-width: 720px;
+      min-width: 720px;
+      margin: 0 auto;
+      .home-index-logo-row {
+        display: flex;
+        justify-content: center;
+        .logo {
+          width: 300px;
+          min-width: 300px;
+          height: 180px;
+          position: relative;
+          margin-left: -20px;
+          &:after {
+            content: '';
+            width: 2px;
+            height: 100%;
+            background-color: $forth-level-border-color;
+            display: block;
+            position: absolute;
+            right: 0;
+            top: 0;
+          }
+        }
+        .home-index-logo-row_text {
+          width: 400px;
+        }
+      }
     }
   }
 </style>
