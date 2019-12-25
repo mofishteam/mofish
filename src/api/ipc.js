61ab37022d1ea243fa8534ea0c4e0dff61b4a78b
wszxdhr <wszxdhr@126.com>
2019-12-25 23:22:37 +0800

    style:去掉ipc代码中的console.log

--- src/api/ipc.js
+++ src/api/ipc.js
@@ -1,10 +1,5 @@
 import { ipcRenderer } from 'electron'
 // const ipcRenderer = window.electron.ipcRenderer
-console.log(require('fs'), window.require('fs'))
-setTimeout(() => {
-  console.log(require('fs'))
-}, 1000)
-// const { ipcRenderer } = require('electron')
 console.log(ipcRenderer)
 export default function (url, data = {}) {
   return new Promise(resolve => {
